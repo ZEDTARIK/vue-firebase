695861e64d3088712a0a054b4e0e49206a86f40d
zouhair <ettarak.zouhair@gmail.com>
2022-02-25 23:09:46 +0100

    first commit

---
+++ src/components/Posts.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+import { ref } from 'vue'
+import { collectionPosts } from '../firebase/confi'
+export default {
+    
+    setup() {
+        
+        const posts = ref([]);
+        const load = async() => {
+            const res = await collectionPosts.collection('posts').get();
+            posts.value = res.docs.map(doc => {
+                return { ...doc.data(), id: doc.id }
+            })
+        }
+
+        load();
+        return { posts, load }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
